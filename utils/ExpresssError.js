0c755040b68afc0a13186cd38d26a195930f2d4f
Rahul Patil <you@example.com>
2024-08-31 20:17:00 +0530

    add project files

---
+++ utils/ExpresssError.js
@@ -0,0 +1,9 @@
+class ExperessError extends Error {
+    constructor(statusCode, message) {
+        super() ;
+        this.statusCode = statusCode ;
+        this.message = message ;
+    }
+}
+
+module.exports = ExperessError ;
